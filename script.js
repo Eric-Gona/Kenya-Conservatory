04a0a7eda9294ef5cc8eea1e4bfc1ae8fe5cc145
Eric-Gona <eric.gona254@gmail.com>
2021-12-01 12:19:47 +0300

    adds files and code

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+$(function()
+{  
+    //value for animation  
+    var width = 900;  
+    var animationSpeed = 1000;  
+    var pause = 3000;  
+    var currentSlide = 1;  
+    //Dom element.   
+    var $slider = $('#slider');  
+    var $sliderAnimation = $slider.find('.slides');  
+    var $slides = $sliderAnimation.find('.slide');  
+  
+    setInterval(function()
+    {  
+         $sliderAnimation.animate({'margin-left': '-='+width}, animationSpeed, function(){  
+         currentSlide ++;  
+         if(currentSlide === $slides.length)  
+         {  
+                $sliderAnimation.css('margin-left', 0);  
+                currentSlide = 1;  
+         }  
+     });  
+    },pause);  
+});  
